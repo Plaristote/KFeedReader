1edb748ce7c27a742f6a679d5337b5bdf6f76c3b
plaristote <michael@unetresgrossebite.com>
2024-05-31 20:54:58 +0200

    add craft blueprints maybe ?

---
+++ craft/KFeedReader/KFeedReader.py
@@ -0,0 +1,33 @@
+import info
+
+class subinfo(info.infoclass):
+    def selfTargets(self):
+        self.displayName = "KFeedReader"
+        self.description = "RSS/Atom/JSON feed aggregator"
+        self.svnTargets["master"] = "https://github.com/Plaristote/KFeedReader.git|kf6"
+        self.defaultTarget = "master"
+    def setDependencies(self):
+        self.buildDependencies["dev-utils/pkg-config"] = "default"
+        self.runtimeDependencies["libs/qt/qtbase"] = "default"
+        self.runtimeDependencies["libs/qt/qtwebengine"] = "default"
+        self.runtimeDependencies["kirigami"] = "default"
+        self.runtimeDependencies["kcoreaddons"] = "default"
+        self.runtimeDependencies["ki18n"] = "default"
+        self.runtimeDependencies["breeze"] = "default"
+        self.runtimeDependencies["kiconthemes"] = "default"
+        if CraftCore.compiler.isMinGW():
+            self.runtimeDependencies["libs/runtime"] = None
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+        CMakePackageBase.buildTests = False
+    def createPackage(self):
+        self.defines["appname"] = "KFeedReader"
+        #self.defines["icon"]
+        #self.defines["shortcuts"]
+        if not CraftCore.compiler.isLinux:
+            self.ignoredPackages.append("libs/dbus")
+        return super().createPackage()
