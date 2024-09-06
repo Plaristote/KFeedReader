e11c01cf8b0d9e7af7467d9a4b599fa1d96e9427
plaristote <michael@unetresgrossebite.com>
2024-09-06 18:02:33 +0200

    fix Craft blueprints

--- craft/KFeedReader/KFeedReader.py
+++ craft/KFeedReader/KFeedReader.py
@@ -1,11 +1,14 @@
 import info
+from CraftCore import CraftCore
 
 class subinfo(info.infoclass):
-    def selfTargets(self):
-        self.displayName = "KFeedReader"
+    def setTargets(self):
+        self.displayName = "FluxKap"
         self.description = "RSS/Atom/JSON feed aggregator"
         self.svnTargets["master"] = "https://github.com/Plaristote/KFeedReader.git|master"
+        self.svnTargets["kf5"] = "https://github.com/Plaristote/KFeedReader.git|kf5"
         self.defaultTarget = "master"
+
     def setDependencies(self):
         self.buildDependencies["dev-utils/pkg-config"] = "default"
         self.runtimeDependencies["libs/qt/qtbase"] = "default"
@@ -21,12 +24,14 @@ class subinfo(info.infoclass):
 from Package.CMakePackageBase import *
 
 class Package(CMakePackageBase):
-    def __init__(self):
-        CMakePackageBase.__init__(self)
+    def __init__(self, **kwargs):
+        CMakePackageBase.__init__(self, **kwargs)
         CMakePackageBase.buildTests = False
+
     def createPackage(self):
-        self.defines["appname"] = "KFeedReader"
-        #self.defines["icon"]
+        self.defines["appname"] = "FluxKap"
+        self.defines["icon"] = self.blueprintDir() / "KFeedReader.ico"                                                     
+        self.defines["icon_png"] = self.sourceDir() / "src/contents/icons/app.png"
         #self.defines["shortcuts"]
         if not CraftCore.compiler.isLinux:
             self.ignoredPackages.append("libs/dbus")
